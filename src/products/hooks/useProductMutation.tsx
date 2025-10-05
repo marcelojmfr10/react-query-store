9bb77f7eb6ff131404db605534b2e3516d1ccc4e
Marcelo Fuentes <marcelofuentes_10@hotmail.com>
2025-10-04 20:06:32 -0600

    Funcionalidad del formulario y useMutation

---
+++ src/products/hooks/useProductMutation.tsx
@@ -0,0 +1,31 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Product, productActions } from "..";
+
+
+export const useProductMutation = () => {
+
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation({
+        mutationFn: productActions.createProduct,
+        onSuccess: (product) => {
+            // queryClient.invalidateQueries({
+            //     queryKey: ['products', { 'filterKey': data.category }]
+            // })
+
+            queryClient.setQueryData<Product[]>(
+                ['products', {'filterKey': product.category}],
+                (old) => {
+                    if (!old) return [product];
+
+                    return [...old, product];
+                },
+            );
+        },
+        // onSettled: () => {
+        //     console.log('on settle');
+        // }
+    });
+
+    return mutation;
+}
--- src/products/services/actions.service.ts
+++ src/products/services/actions.service.ts
@@ -5,7 +5,16 @@ interface GetProductsOptions {
     filterKey?: string;
 }
 
-const sleep = (seconds: number): Promise<boolean> => {
+interface ProductLike {
+    id?: number;
+    title: string;
+    price: number;
+    description: string;
+    category: string;
+    image: string;
+}
+
+export const sleep = (seconds: number): Promise<boolean> => {
     return new Promise(resolve => {
         setTimeout(() => {
             resolve(true)
@@ -14,18 +23,20 @@ const sleep = (seconds: number): Promise<boolean> => {
 }
 
 export const getProducts = async ({ filterKey }: GetProductsOptions): Promise<Product[]> => {
-    await sleep(2);
-
+    // await sleep(2);
     const filterUrl = filterKey ? `category=${filterKey}` : '';
-
     const { data } = await productsApi.get<Product[]>(`/products?${filterUrl}`);
-
     return data;
 }
 
 export const getProductById = async (id: number): Promise<Product> => {
     // await sleep(2);
     const { data } = await productsApi.get<Product>(`/products/${id}`);
+    return data;
+}
 
+export const createProduct = async (product: ProductLike) => {
+    await sleep(5);
+    const { data } = await productsApi.post<Product>(`/products`, product);
     return data;
 }
